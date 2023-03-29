3e44402c91900114a2c7a036a92f0bc0d92fad5a
Karina <karinamello.d@gmail.com>
2023-03-29 00:04:13 +0000

    Add questions Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,22 +12,65 @@ var score = 0;
 
 let questions = [
     {
-       question: "A Cheetah is capable of running at 80 to 98 km/h?",
-       answers: [
-          {option:"Yes!",answer:true},
-          {option:"No!",answer:false}
-        ]
+         question: "A Cheetah is capable of running at 80 to 98 km/h?",
+         answers: [
+             {option:"Yes!",answer:true},
+             {option:"No!",answer:false}
+         ]
 
-      }, 
-      {
+    }, 
+    {
            question: "The adult female Lion is larger than the male",
            answers: [
-              {option:"No!",answer:true},
-              {option:"Yes!",answer:false}
-        ]
+               {option:"No!",answer:true},
+               {option:"Yes!",answer:false}
+           ]
+        
+      },
+      { 
+
           
-          question: "The Anaconda kills its preys with venon?",
+          
+           question: "The Anaconda kills its preys with venon?",
           answers: [
               {option:"No!",answer:true},
               {option:"Yes!",answer:false}
-        
+          ]
+      }
+
+]
+
+restartBtn.addEventListener('click,restart');
+prevBtn.addEventListener('click,prev'); 
+nextBtn.addEventListener('click,next');
+submitBtn.addEventListener(click,submit);
+
+function beginQuiz(){
+    currentQuestion =0;
+    questionText.innerHTML = questions[currentQuestion].question;
+    trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
+    trueBtn.onclick = () => {
+        let ano=0;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score<3){
+                score++;
+            }    
+        }    
+        userScore.innerHTML = score;
+        if(currentQuestion<2){
+             nextBtn();
+        }
+    } 
+    falseBtn.innerHTML = questions[currentQuestion].answers[1].option;
+    falseBtn.onclick = () => {
+        let ano=1;
+        if(questions[currentQuestion].answers[ano].answer){
+            if(score<3){
+                score++;
+            }
+        }        
+        userScore.innerHTML = score;
+        if(currentQuestion<2){
+            nextBtn();
+        }
+    }        
